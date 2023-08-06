0618adf979619ace3e1cc82b5cd5672b32e9b061
Uzair <uzair.muaz3k@gmail.com>
2023-08-06 16:38:40 +0500

    port fixed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 4173, // Use the dynamic port provided by Heroku or a fallback port (e.g., 4173 in this case)
+  },
 })
